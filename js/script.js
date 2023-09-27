7d9873437060402218d5257b6452c7d7d7c520e6
Alethepata <alessia.patane@gmail.com>
2023-09-27 14:11:39 +0200

    [Add] salutoFunzione( )

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-console.log('ciao')
+
 const {createApp} = Vue;
+let message;
 
 createApp({
 
     data(){
         return{
             nome:'Kira',
-            cognome:'The dog'
+            cognome:'The dog',
         }
+    },
+    methods:{
+        salutoFunzione(){
+            return `Ciao ${this.nome} ${this.cognome}` 
+        }
+    },
+    mounted(){
+        salutoFunzione();
     }
-
+   
 }).mount('#app')
